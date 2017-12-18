adac9c4304e9c27655b012272a1c039f87fdb571
jsmith121 <jsmth8483@gmail.com>
2017-12-18 12:06:16 -0800

    Navigation bar added. Style changes. Database controller set up to modify local SQLite database.

--- app/src/main/java/com/magpiehunt/magpie/MainActivity.java
+++ app/src/main/java/com/magpiehunt/magpie/MainActivity.java
@@ -1,31 +1,44 @@
 package com.magpiehunt.magpie;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.nfc.Tag;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
+
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+import com.magpiehunt.magpie.Fragments.MapFragment;
+import com.magpiehunt.magpie.Fragments.MyCollectionsFragment;
+import com.magpiehunt.magpie.Fragments.PrizesFragment;
+import com.magpiehunt.magpie.Fragments.QRFragment;
+import com.magpiehunt.magpie.Fragments.SearchCollectionsFragment;
 
 /**
  * Author:  Blake Impecoven
  * Date:    11/14/17.
  */
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, MyCollectionsFragment.OnFragmentInteractionListener, MapFragment.OnFragmentInteractionListener, QRFragment.OnFragmentInteractionListener, SearchCollectionsFragment.OnFragmentInteractionListener,PrizesFragment.OnFragmentInteractionListener {
 
     private static final String TAG = "MainActivity";
 
+    private BottomNavigationView bottomNavigationView;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
     /*
      * Firebase/Google instance variables
     **/
@@ -46,11 +59,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+
 
         // Initialize Firebase
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        if (mFirebaseUser == null) {
+       /* if (mFirebaseUser == null) {
             // Not signed in, launch the Sign In activity
             startActivity(new Intent(this, SignInActivity.class));
             finish();
@@ -71,12 +88,61 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API)
                 .build();
-
+*/
         // Firebase Database Initialization - Soon to come...
 //        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
+        //Create bottom navigation bar to switch between app pages
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        setupFragments();
+
+        SQLiteDatabase mydatabase = openOrCreateDatabase("magpie.db",MODE_PRIVATE,null);
 
     }//end onCreate
+
+    //this method creates the fragments for each page accessible from the bottom navigation
+    // bar and sets up the listener for the navigation bar.
+    private void setupFragments()
+    {
+        final MyCollectionsFragment myCollectionsFragment = new MyCollectionsFragment();
+        final MapFragment mapFragment = new MapFragment();
+        final QRFragment qrFragment = new QRFragment();
+        final SearchCollectionsFragment searchCollectionsFragment = new SearchCollectionsFragment();
+        final PrizesFragment prizesFragment = new PrizesFragment();
+        fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.fragment_container, myCollectionsFragment).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        FragmentTransaction replace;
+                        fragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.menu_map:
+                                fragment = mapFragment;
+                                break;
+                            case R.id.menu_qr:
+                                fragment = qrFragment;
+                                break;
+                            case R.id.menu_home:
+                                fragment = myCollectionsFragment;
+                                break;
+                            case R.id.menu_search:
+                                fragment = searchCollectionsFragment;
+                                break;
+                            case R.id.menu_prizes:
+                                fragment = prizesFragment;
+                                break;
+                        }
+                        replace = fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment);
+                        replace.addToBackStack(null);
+                        replace.commit();
+                        return true;
+                    }
+                });
+    }
 
     @Override
     public boolean onOptionsItemSelected (MenuItem item) {
@@ -106,4 +172,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
---
+++ app/src/main/java/com/magpiehunt/magpie/Model/Item.java
@@ -0,0 +1,9 @@
+package com.magpiehunt.magpie.Model;
+
+/**
+ * Created by James on 12/15/2017.
+ */
+
+public class Item {
+    
+}
