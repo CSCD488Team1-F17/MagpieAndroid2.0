c29e4829fb209f02dcc7078333cda9b21301d9f3
jsmith121 <jsmth8483@gmail.com>
2018-01-15 12:53:22 -0800

    Room database built

--- app/src/main/java/com/magpiehunt/magpie/DataAccess/CollectionDao.java
+++ app/src/main/java/com/magpiehunt/magpie/DataAccess/CollectionDao.java
@@ -1,7 +1,14 @@
 package com.magpiehunt.magpie.DataAccess;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.magpiehunt.magpie.Entities.Collection;
+
+import java.util.List;
 
 /**
  * Created by James on 1/12/2018.
@@ -9,5 +16,19 @@ import android.arch.persistence.room.Query;
 
 @Dao
 public interface CollectionDao {
+
+    @Query("SELECT * FROM Collections WHERE Available = 1")
+    List<Collection> getCollections();
+    @Query("SELECT * FROM Collections WHERE CID = :cid")
+    Collection getCollection(int cid);
+    @Update
+    void updateCollection(Collection... collections);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void addCollection(Collection... collections);
+
+
+
+
 
 }
